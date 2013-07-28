0fb7ee78d1d419e04124aeb41e822f9e2151d486
Mathieu Bridon <bochecha@fedoraproject.org>
2013-07-28 21:50:39 +0800

    Add the filter flags
    Applications will use these to specify which characters they are
    interested in.
    
    They correspond exactly to the columns in the 'chars' table of our
    database.

---
+++ src/cangjie.h
@@ -0,0 +1,31 @@
+/* Copyright (c) 2013 - The libcangjie2 authors.
+ *
+ * This file is part of libcangjie2.
+ *
+ * libcangjie2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libcangjie2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libcangjie2.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+typedef enum CangjieFilter {
+    CANGJIE_FILTER_BIG5        = 1 << 0,
+    CANGJIE_FILTER_HKSCS       = 1 << 1,
+    CANGJIE_FILTER_PUNCTUATION = 1 << 2,
+    CANGJIE_FILTER_TRADITIONAL = 1 << 3,
+    CANGJIE_FILTER_SIMPLIFIED  = 1 << 4,
+    CANGJIE_FILTER_ZHUYIN      = 1 << 5,
+    CANGJIE_FILTER_KANJI       = 1 << 6,
+    CANGJIE_FILTER_KATAKANA    = 1 << 7,
+    CANGJIE_FILTER_HIRAGANA    = 1 << 8,
+    CANGJIE_FILTER_SYMBOLS     = 1 << 9,
+} CangjieFilter;
