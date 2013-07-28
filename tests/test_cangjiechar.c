3af0c062d62d73806450f2d7aee366b05d6fc0e3
Mathieu Bridon <bochecha@fedoraproject.org>
2013-07-28 18:46:38 +0800

    tests: Add some unit tests of CangjieChar

---
+++ tests/test_cangjiechar.c
@@ -0,0 +1,57 @@
+/* Copyright (c) 2013 - The libcangjie2 authors.
+ *
+ * This file is part of libcangjie2.
+ *
+ * libcangjie2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libcangjie2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libcangjie2.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <assert.h>
+#include <string.h>
+
+#include <cangjiechar.h>
+
+
+void test_cangjie_char_a() {
+    char *chchar = "a";
+    char *code = "abc";
+    uint32_t classic_freq = 123;
+
+    CangjieChar *c = cangjie_char_new(chchar, code, classic_freq);
+
+    assert(strcmp(c->chchar, chchar) == 0);
+    assert(strcmp(c->code, code) == 0);
+    assert(c->classic_freq == classic_freq);
+
+    cangjie_char_free(c);
+}
+
+void test_cangjie_char_zh() {
+    char *chchar = "木";
+    char *code = "abc";
+    uint32_t classic_freq = 123;
+
+    CangjieChar *c = cangjie_char_new(chchar, code, classic_freq);
+
+    assert(strcmp(c->chchar, chchar) == 0);
+    assert(strcmp(c->code, code) == 0);
+    assert(c->classic_freq == classic_freq);
+
+    cangjie_char_free(c);
+}
+
+
+int main() {
+    test_cangjie_char_a();
+    test_cangjie_char_zh();
+}
