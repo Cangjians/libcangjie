323ac03e0fc670d63a3a8589157016f83857872b
Mathieu Bridon <bochecha@fedoraproject.org>
2013-08-13 16:02:08 -0400

    errors: Add a new error code for database errors

--- src/cangjieerrors.h
+++ src/cangjieerrors.h
@@ -1,2 +1,3 @@
 #define CANGJIE_OK  0  /* Successful result */
 #define CANGJIE_NOCHARS  1  /* Nothing wrong, but no corresponding CangjieChar returned */
+#define CANGJIE_DBERROR  2  /* Something happened when querying the database */
