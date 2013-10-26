5db82229877d3cee4b5c642ee64eabedf56d314d
Mathieu Bridon <bochecha@fedoraproject.org>
2013-10-26 10:39:53 +0800

    cli: Add missing copyright header
    Can't believe we never had this. o_O

--- src/cli.c
+++ src/cli.c
@@ -1,3 +1,22 @@
+/* Copyright (c) 2013 - The libcangjie authors.
+ *
+ * This file is part of libcangjie.
+ *
+ * libcangjie is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libcangjie is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libcangjie.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #include <getopt.h>
 #include <stdio.h>
 #include <string.h>
